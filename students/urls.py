ea7725a37a067ec514cbbbc7f3e7cd72cb29b98b
rachitpoudel101 <rachit300poudel@gmail.com>
2024-08-15 14:04:03 +0545

    i have made the add student and save it into database and we can see the number of students in database.

--- students/urls.py
+++ students/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name= 'index'),
+    path('<int:id>',views.view_student, name= 'view_student'),
+    path('add/', views.add, name='add'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from .models import Student
+from .forms import StudentForm
+
 def index(request):
     students = Student.objects.all()
-    return render(request, 'students/index.html',{'Students': students})
+    return render(request, 'students/index.html', {'students': students})
+
+def view_student(request, id):
+    student = get_object_or_404(Student, pk=id)
+    return render(request, 'students/view_student.html', {'student': student})
+
+def add(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the form data to the model
+            return render(request, 'students/add.html', {
+                'form': StudentForm(),
+                'success': True
+            }) 
+    else:
+        form = StudentForm()
+        
+    return render(request, 'students/add.html', {
+        'form': form
+    })
